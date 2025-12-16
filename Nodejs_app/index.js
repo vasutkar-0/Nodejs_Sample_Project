d29ffbc9f693012982dfabd91beacf81811b5500
Vaishnavi <vaishnavi.asutkar@pinnacle.in>
2025-12-16 17:17:45 +0530

    Webhook.

--- Nodejs_app/index.js
+++ Nodejs_app/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({
-    message: 'Node.js app deployed successfully. 🚀',
+    message: '*Node.js app deployed successfully* 🚀',
     hostname: require('os').hostname(),
     timestamp: new Date().toISOString()
   }));
